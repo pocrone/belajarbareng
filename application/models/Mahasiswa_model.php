dcbfc375db9ed0917ffe6ec092b20756a856123b
mas-iybaz <muhammadiqbal.workspace@gmail.com>
2020-05-11 15:49:25 +0700

    add basic crud query

--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -7,4 +7,26 @@ class Mahasiswa_Model extends CI_Model
     {
         return $this->db->get();
     }
+
+    public function getUser($id)
+    {
+        return $this->db->get_where('mahasiswa', ['id' => $id])->row_array();
+    }
+
+    public function insert($user)
+    {
+        $this->db->insert('mahasiswa', $user);
+    }
+
+    public function update($id, $data)
+    {
+        $this->db->where('id', $id);
+        return $this->db->update('mahasiswa', $data);
+    }
+
+    public function delete($id)
+    {
+        $this->db->where('id', $id);
+        return $this->db->delete('mahasiswa');
+    }
 }
