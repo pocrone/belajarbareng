48d16ebf7139d118a5941597137c93a0e3b24275
mas-iybaz <muhammadiqbal.workspace@gmail.com>
2020-05-11 14:15:25 +0700

    load model

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -6,10 +6,29 @@ class Mahasiswa extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model('Mahasiswa_model');
     }
 
     public function index()
     {
+        // load view here
+
         echo 'Mahasiswa';
+    }
+
+    public function tambahMahasiswa()
+    {
+        // load view here
+    }
+
+    public function updateMahasiswa()
+    {
+        // load view here
+    }
+
+    public function hapusMahasiswa()
+    {
+        // run query
     }
 }
