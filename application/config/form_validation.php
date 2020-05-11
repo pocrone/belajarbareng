643219b9b29ce78f44645a32075c4d914b89fc1e
mas-iybaz <muhammadiqbal.workspace@gmail.com>
2020-05-11 15:48:27 +0700

    add form validation

---
+++ application/config/form_validation.php
@@ -0,0 +1,60 @@
+<?
+
+$config = array(
+    'mahasiswa/tambahMahasiswa' => array(
+        array(
+            'field' => 'name',
+            'label' => 'Name',
+            'rules' => 'required|trim',
+            'errors' => array(
+                'required' => 'Nama wajib diisi'
+            )
+        ),
+        array(
+            'field' => 'email',
+            'label' => 'Email',
+            'rules' => 'required|trim|valid_email|is_unique[mahasiswa.email]',
+            'errors' => array(
+                'required' => 'Email wajib diisi',
+                'valid_email' => 'Isi dengan email valid',
+                'is_unique' => 'Email sudah terdaftar'
+            )
+        ),
+        array(
+            'field' => 'program_studi',
+            'label' => 'Program Studi',
+            'rules' => 'required|trim',
+            'errors' => array(
+                'required' => 'Program studi wajib diisi'
+            )
+        ),
+    ),
+    'mahasiswa/updateMahasiswa' => array(
+        array(
+            'field' => 'name',
+            'label' => 'Name',
+            'rules' => 'required|trim',
+            'errors' => array(
+                'required' => 'Nama wajib diisi'
+            )
+        ),
+        array(
+            'field' => 'email',
+            'label' => 'Email',
+            'rules' => 'required|trim|valid_email|is_unique[mahasiswa.email]',
+            'errors' => array(
+                'required' => 'Email wajib diisi',
+                'valid_email' => 'Isi dengan email valid',
+                'is_unique' => 'Email sudah terdaftar'
+            )
+        ),
+        array(
+            'field' => 'program_studi',
+            'label' => 'Program Studi',
+            'rules' => 'required|trim',
+            'errors' => array(
+                'required' => 'Program studi wajib diisi'
+            )
+        ),
+    )
+);
